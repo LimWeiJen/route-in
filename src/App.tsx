aa761950867034684179d49ad82f4007b4caced5
Lim Wei Jen <limweijen96@gmail.com>
2022-09-05 14:33:05 +0800

    feat: Added authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
 import { auth } from './firebase';
 import './App.css'
+import { useEffect } from 'react';
 
 function App() {
+	useEffect(() => { if (auth.currentUser) location.href = '/home' });
+
 	const signIn = () => {
 		signInWithPopup(auth, new GoogleAuthProvider()).then(() => {
 			location.href = '/home';
