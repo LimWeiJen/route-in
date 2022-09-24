faea84c396e886dc897198bd18a248ac7b2d086a
Lim Wei Jen <limweijen96@gmail.com>
2022-09-24 17:03:25 +0800

    feat: Added ability to fetch task groups by day:

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Navbar, Sidebar } from './components'
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
+import { Link } from 'react-router-dom';
+import { auth, db } from './firebase';
+import { TaskGroup, User } from './interfaces';
 
 const Home = () => {
+  const [taskGroups, setTaskGroups] = useState<Array<TaskGroup>>();
+  
+  auth.onAuthStateChanged(async (user) => {
+    if (!user) return;
+
+    // get user data
+    const userDoc = await getDoc(doc(db, 'users', user.uid));
+    const userData: User = JSON.parse(JSON.stringify(userDoc.data()));
+
+    setTaskGroups(userData.taskGroups);
+  })
+
+  const getTodaysDay = () => new Date().getDay()
+
   return (
     <div>
       <Navbar />
       <Sidebar />
-      Home
+      {taskGroups?.filter(taskGroup => taskGroup.dayOfAppearance[getTodaysDay()]).map((taskGroup, i) => <div key={i}>
+        {taskGroup.name}
+        {taskGroup.tasks.map((task, j) => <div key={j}>
+          {task.checked}
+          {task.completionRate}
+          <input type="checkbox" checked={task.checked} />
+        </div>)}
+      </div>)}
     </div>
   )
 }
