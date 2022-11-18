b17c5d46ad86542f9d69c88f82357f2e551c7d70
Lim Wei Jen <limweijen96@gmail.com>
2022-11-18 09:19:33 +0800

    style: Added img in product landing page

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { doc, getDoc, setDoc } from 'firebase/firestore';
 import { User } from './interfaces';
 import './styles/homepage.scss'
 import logo from './images/Routine Tracker.svg'
-import backgroundImg from './images/Background Image.svg'
+import backgroundImg from './images/Background Image 2.png'
 
 function App() {
 	auth.onAuthStateChanged(async (user) => {
@@ -46,7 +46,6 @@ function App() {
 
 	return (
 		<div className='background'>
-			<img className='bg-img' src={backgroundImg} alt="" />
 			<div className='navbar'>
 				<div className='left'>
 					<img src={logo} />
@@ -57,24 +56,30 @@ function App() {
 				</div>
 			</div>
 			<div className='heading'>
-				<div className='heading-container'>
-					<div>
-						<h1>You Are What You Repeatedly Do</h1>
-						<p>routine tracker and habit builder to get you back on track with your life</p>
+				<div className='heading-left'>
+					<div className='heading-container'>
+						<div>
+							<h1>You Are What You Repeatedly Do</h1>
+							<p>routine tracker and habit builder to get you back on track with your life</p>
+						</div>
+						<div>
+							<button onClick={_login}>Get Started</button>
+						</div>
+						<p>created by @limweijen ^_^</p>
 					</div>
-					<div>
-						<button onClick={_login}>Get Started</button>
-					</div>
-					<p>created by @limweijen ^_^</p>
 				</div>
+				<img className='bg-img' src={backgroundImg} alt="" />
 			</div>
+			<div className='divider'></div>
 			<footer>
 				<div className='socials'>
 					<div>Lim Wei Jen</div>
 					<div>
-						<p>Follow me on <a href="">Instagram</a></p>
+						<p>Follow me on <a href="https://www.instagram.com/limweijen96/" target="_blank">Instagram</a></p>
 						<br />
-						<p>Support me on <a href="">Github</a></p>
+						<p>Support me on <a href="https://github.com/LimWeiJen" target="_blank">Github</a></p>
+						<br />
+						<p>Get in touch with me on <a href="discordapp.com/users/974579792248700938" target="_blank">Discord</a></p>
 						<br />
 						<div>
 							<p>Email</p>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,8 @@
-import { signOut } from 'firebase/auth'
 import { auth } from '../firebase'
 import React, { useContext } from 'react'
 import { UserContext } from '../contexts/UserContext'
 import '../styles/navbar.scss'
-import { PlusCircle, ToggleLeft } from 'react-feather'
+import { PlusCircle, ToggleLeft, ToggleRight } from 'react-feather'
 import logo from '../images/Routine Tracker.svg'
 import logoDark from '../images/Routine Tracker Dark.svg'
 
@@ -18,7 +17,10 @@ const Navbar = () => {
       </div>
       <div className='right'>
         <PlusCircle className='nav-btn ico' onClick={userContext?.addNewTaskGroup} />
-        <ToggleLeft className='nav-btn ico' onClick={() => userContext?.switchTheme(userContext.theme === 'light' ? 'dark' : 'light')} />
+        {userContext?.theme === 'light' ? 
+        <ToggleLeft className='nav-btn ico' onClick={() => userContext?.switchTheme(userContext.theme === 'light' ? 'dark' : 'light')} /> : 
+        <ToggleRight className='nav-btn ico' onClick={() => userContext?.switchTheme(userContext.theme === 'light' ? 'dark' : 'light')} />
+        }
         <img src={auth.currentUser?.photoURL || ''} />
       </div>
     </div>
